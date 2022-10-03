21b3fdd7cd2339ad73ac6cb71d71de437b4ba785
Nikita Polozov <npolozov0@gmail.com>
2022-10-03 13:41:49 +0300

    !

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,15 @@
 const form = document.querySelector(".login-form");
 
+
+const formElements = event.currentTarget.elements;
+
+const email = formElements.email.value;
+const password = formElements.password.value;
+
 form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-
-  const formElements = event.currentTarget.elements;
-
-  const email = formElements.email.value;
-  const password = formElements.password.value;
 
   if (email === "" || password === "") {
     return alert("Alert");
